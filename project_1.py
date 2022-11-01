6a04ed2ea62f15c17c054fbef60eb69267e12943
HZ004 <97055360+HZ004@users.noreply.github.com>
2022-11-01 15:20:44 +0530

    Update project_1.py

--- project_1.py
+++ project_1.py
@@ -21,7 +21,7 @@ Original file is located at
 
 import pandas as pd
 import numpy as np
-import seaborn as sns
+# import seaborn as sns
 from datetime import datetime
 from matplotlib import pyplot as plt
 import warnings
