176298c55b84e812fb0a99609b3c51021aca0bc1
HZ004 <97055360+HZ004@users.noreply.github.com>
2022-11-02 23:53:40 +0530

    Update project_1.py

--- project_1.py
+++ project_1.py
@@ -15,7 +15,7 @@ from tvDatafeed import TvDatafeed ,Interval
 import fbprophet
 from fbprophet import Prophet
 
-COMPANY = NIFTY_50
+COMPANY = 'NIFTY'
 
 st.title('Model Deployment: Forecasting')
 st.sidebar.header('Input Company symbol listed on NSE')
